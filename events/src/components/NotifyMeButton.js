4b91c9877d87ef170f27b533bcd5fa5ffd06f686
K Advaith <162140222+KambhampatiAdvaith@users.noreply.github.com>
2025-09-20 20:23:43 +0530

    Add files via upload

---
+++ events/src/components/NotifyMeButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NotifyMeButton.css';
+
+function NotifyMeButton() {
+    return (
+        <button className="notify-me-button">
+            <span className="bell-icon"></span> Notify Me
+        </button>
+    );
+}
+
+export default NotifyMeButton;
