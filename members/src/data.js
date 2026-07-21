f1e2855491e0eaaea85049e2eaed46bb06dc95e7
Rishith Kumar Guntuka <spargy4050v@gmail.com>
2026-07-21 22:02:25 +0530

    feat(members): add club members data
    Signed-off-by: Rishith Kumar Guntuka <spargy4050v@gmail.com>

---
+++ members/src/data.js
@@ -0,0 +1,73 @@
+export const faculty = [
+  {
+    name: "Harish Chandra Reddy",
+    role: "Vice Chairman and Advisor of the OSS",
+  },
+  {
+    name: "D S V Jyothi",
+    role: "Faculty Coordinator of OSS",
+  },
+];
+
+export const advisors = [
+  { name: "Jayatheerth", role: "Advisor", previousRole: "President" },
+  { name: "Advaith", role: "Advisor", previousRole: "Vice President" },
+  { name: "Hruthik Sai", role: "Advisor", previousRole: "Project Department" },
+  { name: "Nyneeta", role: "Advisor", previousRole: "Events and PR" },
+  { name: "M Sai Sree Harsha", role: "Advisor", previousRole: "Treasury Department" },
+  { name: "Aparna Reddy Duddekunta", role: "Advisor", previousRole: "Newsletter" },
+  { name: "Harshini Mungara", role: "Advisor", previousRole: "" },
+  { name: "Sainni Sai Sanjana", role: "Advisor", previousRole: "" },
+  { name: "Sujith", role: "Advisor", previousRole: "" },
+];
+
+export const coreCommittee = [
+  { name: "Bhuvan K", role: "President" },
+  { name: "Rishith Kumar G", role: "Vice President" },
+];
+
+export const departments = [
+  {
+    name: "Project Management",
+    lead: { name: "B V S Pavan Sreekar", role: "Department Lead" },
+    members: [],
+  },
+  {
+    name: "Newsletter and Documentation",
+    lead: { name: "Aishwarya Meher M", role: "Department Lead" },
+    members: [{ name: "Sushanth", role: "Maintainer" }],
+  },
+  {
+    name: "PR, Events and Treasury",
+    lead: { name: "Srinadh D", role: "Department Lead" },
+    members: [],
+  },
+  {
+    name: "HR",
+    lead: { name: "P Keerthi", role: "Department Lead" },
+    members: [],
+  },
+  {
+    name: "Social Media and Gallery",
+    lead: { name: "Harsha Vardhan S", role: "Department Lead" },
+    members: [],
+  },
+];
+
+export const maintainers = [
+  { name: "Emmanuel M J", team: "VERTEX", role: "Team Lead" },
+  { name: "Tarun J Y", team: "SYNC", role: "Team Lead" },
+  { name: "Abdul", team: "HASHERS", role: "Team Lead" },
+  { name: "Aryan", team: "PRACTICAL-PIONEERS", role: "Team Lead" },
+];
+
+export const contributors = [
+  { name: "Aarav Singh", team: "VERTEX", role: "Member" },
+  { name: "Shantan Dheer", team: "VERTEX", role: "Member" },
+  { name: "Amritha Malapaka", team: "SYNC", role: "Member" },
+  { name: "Athul", team: "SYNC", role: "Member" },
+  { name: "Prateek", team: "HASHERS", role: "Member" },
+  { name: "Rohith", team: "HASHERS", role: "Member" },
+  { name: "Amodini", team: "PRACTICAL-PIONEERS", role: "Member" },
+  { name: "Samvedya Sripada", team: "PRACTICAL-PIONEERS", role: "Member" },
+];
